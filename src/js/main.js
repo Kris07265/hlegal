481590bdaf8a18c436051dd3982a9011244afe68
Kris <krasnovkristian07@gmail.com>
2025-10-03 18:38:27 +0300

    added tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,35 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
 
 (() => {
-  const burgerButton = document.querySelector('.header__box-burger');
-  const closeMenuButton = document.querySelector('.header__nav-close');
-  const headerNavigation = document.querySelector('.header__nav');
+  const burgerButton = document.querySelector(".header__box-burger");
+  const closeMenuButton = document.querySelector(".header__nav-close");
+  const headerNavigation = document.querySelector(".header__nav");
 
-  burgerButton.addEventListener('click', () => {
-    headerNavigation.classList.add('active');
+  burgerButton.addEventListener("click", () => {
+    headerNavigation.classList.add("active");
   });
 
-  closeMenuButton.addEventListener('click', () => {
-    headerNavigation.classList.remove('active');
+  closeMenuButton.addEventListener("click", () => {
+    headerNavigation.classList.remove("active");
+  });
+})();
+
+(() => {
+  const tabItems = document.querySelectorAll(".tabs__links-item");
+  const tabContent = document.querySelectorAll(".tabs__content-item");
+
+  tabItems.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabContent.forEach((content) => {
+        content.classList.remove("active");
+      });
+
+      tabItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      tabItems[index].classList.add("active");
+      tabContent[index].classList.add("active");
+    });
   });
 })();
